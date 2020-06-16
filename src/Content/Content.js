ed8f2eb4c5f159b4cb528b98331bd92950cf8ef4
Jakub Kniaz <kukn@wp.pl>
2020-06-16 13:09:00 +0200

    changed Content for Carousel

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import {withRouter, NavLink} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
+import Carousel from "./Carousel";
 
 
 const Content = props => {
-
-    const handleClick = ( type) => {
-        props.history.push({pathname: "/products/" + type})
-    }
-
     return (
         <>
-            <section className="carousel container">
-                <div className="row">
-                    <div className="carousel-content col-10">
-                        <i className="fas fa-chevron-left left-arrow"></i>
-                        <ul>
-                            <li className="visible-poem">
-                                <div className="first-poem"><span>Góry uczą smaku rzeczy prostych,<br/> takich, których nie doceniamy.<br/><br/>Stanisław Biel</span>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="second-poem"><span>Góry są miejscem, gdzie przyrodę tej planety doświadczamy najzupełniej.<br/> Dlatego sam pobyt w górach może stać się wielkim odkryciem.<br/><br/> Wojtek Kurtyka</span>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="third-poem"><span>Góry dają człowiekowi, poprzez zdobywanie wzniesień nieograniczony kontakt z przyrodą<br/> – poczucie
-                        wewnętrznego wyzwolenia, oczyszczenia, niezależności.<br/><br/>Jan Paweł II</span></div>
-                            </li>
-                        </ul>
-                        <i className="fas fa-chevron-right right-arrow"></i>
-                    </div>
-                </div>
-            </section>
+            <Carousel />
             <section className="container">
                 <div className="row">
                     <div className="promo-section-first col-12">
